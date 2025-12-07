d88d04a74f3f655dc00ba8b9d9e2a24138ee0ff0
Evgheni Nujnenco <nev.front.dev@gmail.com>
2025-12-07 12:08:15 +0200

    updated

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NVRYUsOJnekDmyavZRg6jCxDFkONffdJufXfSgSgwhA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"US0O41UUalwDSXwxTwIxdzvEBLp18/jOgi54VUdzHdk=\"\n}"
