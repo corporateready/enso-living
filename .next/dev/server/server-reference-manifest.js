902cf93f2454e5e9539b690ba4f1657e963fe585
Evgheni Nujnenco <nev.front.dev@gmail.com>
2025-12-07 16:35:09 +0200

    added netlify.toml file updated

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"US0O41UUalwDSXwxTwIxdzvEBLp18/jOgi54VUdzHdk=\"\n}"
--- .next/dev/static/chunks/app_terms-and-conditions_page_jsx_09e1ca47._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_90e85963._.js"
-],
-    source: "dynamic"
-});
