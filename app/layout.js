cae1519f561ceb37024c03b808a3c888ff0d7238
Evgheni Nujnenco <nev.front.dev@gmail.com>
2025-12-07 17:30:54 +0200

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -11,11 +11,41 @@ const inter = Inter({
 export const metadata = {
   title: "ENSO LIVING",
   description: "ENSO LIVING - Your home for sustainable living",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
+      <head>
+        <meta name="robots" content="index, follow"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
